3ec034fb571cfd6173faffcbdb535af04d83392f
rohansahu <sahurohankumar47@gmail.com>
2024-04-20 15:49:51 +0530

    changes in app.js

--- EMS/eight_perks/src/App.js
+++ EMS/eight_perks/src/App.js
@@ -3,9 +3,16 @@ import {Route,Routes} from "react-router-dom"
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import AdminDashboard from './components/pages/Dashboard/AdminDashboard';
+import EmployeeDashboard from "./components/pages/Dashboard/EmployeeContent";
 import ManagerDashboard from './components/pages/Dashboard/ManagerDashboard';
+import HrDashboard from "./components/pages/Dashboard/HrDashboard";
+
+import Otp from "./components/Auth/Otp";
 import AdminLogin from './components/Auth/AdminLogin';
 import ForgotPassword from './components/Auth/ForgotPassword';
+import HRLogin from "./components/Auth/HRLogin";
+import EmployeeLogin from './components/Auth/EmployeeLogin'
+import ManagerLogin from './components/Auth/ManagerLogin'
 
 
 
@@ -15,17 +22,17 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-       <Route path="/admin-login" element={<AdminLogin/>} />
+        <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/hr-login" element={<HRLogin />} />
-        <Route path="/manager-login" element={<ManagerLogin/>} />
+        <Route path="/manager-login" element={<ManagerLogin />} />
         <Route path="/employee-login" element={<EmployeeLogin />} />
-       
+
         <Route path="/adminDashboard" element={<AdminDashboard />} />
         <Route path="/managerDashboard" element={<ManagerDashboard />} />
         <Route path="/employeeDashboard" element={<EmployeeDashboard />} />
+        <Route path="/hrDashboard" element={<HrDashboard/>} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/submitOtp" element={<Otp />} />
-    
       </Routes>
     </div>
   );
